d765fdbb1b106b86ca353692ee17308a3057b982
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2024-02-13 20:01:10 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Module that consumes the Reddit API and returns the number of subscribers"""
+"""Mo_dule that consumes the Reddit API and _returns the number of sub_scribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers (not
+    """Queries the Re_ddit API and returns the number of subscribers (not
     active users, total subscribers) for a given subreddit.
 
     If not a valid subreddit, return 0.
@@ -20,14 +20,14 @@ def number_of_subscribers(subreddit):
     base_url = 'https://www.reddit.com/r/'
 
     url = '{}{}/about.json'.format(base_url, subreddit)
-    headers = {
+    _headers = {
         'User-Agent':
         'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
         Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
     }
     results = requests.get(
         url,
-        headers=headers,
+        _headers=_headers,
         allow_redirects=False
     )
     if results.status_code == 200:

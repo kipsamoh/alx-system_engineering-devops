9b2a2840f41ca75ddf3a823f553557d593394612
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2024-02-13 20:18:54 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -20,14 +20,14 @@ def number_of_subscribers(subreddit):
     base_url = 'https://www.reddit.com/r/'
 
     url = '{}{}/about.json'.format(base_url, subreddit)
-    _headers = {
+    headers = {
         'User-Agent':
         'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
         Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
     }
     results = requests.get(
         url,
-        _headers=_headers,
+        headers=headers,
         allow_redirects=False
     )
     if results.status_code == 200:

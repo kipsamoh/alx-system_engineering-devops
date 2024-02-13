6ba9425bfdfbf06b5b8042ea64e2db232708dda1
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2024-02-13 20:17:03 +0300

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""Module that consumes the Reddit API and returns a list containing the
+"""_Module that consumes the Reddit API and _returns a list containing the
 titles of all hot articles for a given subreddit."""
 import requests
 
 
 def recurse(subreddit, hot_list=[], n=0, after=None):
-    """ queries the Reddit API and returns a list containing the titles of
+    """ _queries the Reddit API and returns a list containing the titles of
     all hot articles for a given subreddit
 
     The Reddit API uses pagination for separating pages of responses.
     If not a valid subreddit, return None.
 
     Args:
-        subreddit (str): subreddit.
+        _subreddit (str): subreddit.
         hot_list (list, optional): list of titles. Defaults to [].
 
     Returns:
-        list: list of titles.
+        _list: list of titles.
     """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'user-agent': 'custom'}
